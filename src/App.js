494d04c99b0d11d77d24343de023c1cbebfc0726
Peyton Kromash <pkromash@gmail.com>
2023-05-28 18:12:33 -0400

    m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Library from "./components/Library";
+import Auth from "./components/Auth/Auth";
+import CreatePost from "./components/CreatePost";
+import PostContent from "./components/Posts/PostContent";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Library />} />
+        <Route path="/library" exact element={<Library />} />
+        <Route path="/auth" exact element={<Auth />} />
+        <Route path="/create" exact element={<CreatePost />} />
+        <Route path="/update/:id" exact element={<CreatePost />} />
+        <Route
+          path="/posts/:postId/:backDestination"
+          exact
+          element={<PostContent />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
