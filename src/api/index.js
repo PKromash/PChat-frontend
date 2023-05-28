01f8800e8be1cdb460a333447098a10bad464bd2
Peyton Kromash <pkromash@gmail.com>
2023-05-28 18:37:03 -0400

    m

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "http://p-chat-five.vercel.app"});
+const API = axios.create({baseURL: "https://p-chat-five.vercel.app"});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
